e702efc525189e35d9f4bad516b733816974a874
Jakub Nowacki <j.s.nowacki@gmail.com>
2015-10-19 22:56:58 +0200

    Word Count Streaming added

---
+++ streaming/src/main/java/pl/com/sages/spark/WordCountStreaming.java
@@ -0,0 +1,35 @@
+package pl.com.sages.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import pl.com.sages.spark.conf.SparkConfBuilder;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+/**
+ * Word Count Java 8
+ */
+public class WordCountStreaming {
+
+    public static void main(String[] args) {
+        SparkConf conf = SparkConfBuilder.buildLocal("word-count-streaming");
+
+        JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(1));
+        sc.checkpoint("checkpoint/");
+
+        JavaDStream<String> file = sc.textFileStream(args[0]);
+        JavaDStream<String> words = file.flatMap(s -> Arrays.asList(s.split(" ")));
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> counts = pairs.reduceByKey((a, b) -> a + b);
+        counts.print();
+
+        sc.start();
+        sc.awaitTermination();
+    }
+}
